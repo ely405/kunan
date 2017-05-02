9d599573b78b7dee5229992483864b91b56076d5
ely211 <gonzalesm405@gmail.com>
2017-05-02 02:39:53 -0500

    button-css

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,20 +20,64 @@ function createInputCheck(arrayToCreateInputCheck, classToParentCreated, parentT
     parentToApendChild.append(docFrag);
 }
 
-window.addEventListener("load", function(){
-    createInputCheck(checkRegiones, "div-check-input", divChkRegiones);
-    createInputCheck(checkSocialSector, "div-check-input", divSocialSector);
-});
+function capitalLetter(){
+  var inputLetter;
+  if(event.target.getAttribute("type") == "text"){
+    inputLetter = event.target.value.split(" ");
+    var mapingArr = inputLetter.map((element) => (element.charAt(0).toUpperCase() + element.substring(1, element.length).toLowerCase()));
+    event.target.value = mapingArr.toString().replace(",", " ");
+  }
+}
+
+var parent;
+
+function createTooltip(parentElement, errorMessage){
+    if(parentElement.lastElementChild.getAttribute("class") =="tooltiptext") {
+      parentElement.lastElementChild.innerHTML = errorMessage;
+      parentElement.lastElementChild.style.display = "block";
+    } else {
+      var span = document.createElement("span");
+      span.innerText = errorMessage;
+      span.classList.add("tooltiptext");
+      parentElement.append(span);
+    }
+}
+
+function onlyLetter(errorMessage){
+  var e = event || window.event;
+  var keycode = e.keyCode || e.charCode;
+  parent = e.target.parentElement;
+  console.log(keycode);
+  (keycode >= 65 && keycode <= 90 || keycode == 192 || keycode == 8 || keycode == 9)? e: (e.preventDefault(), createTooltip(parent, errorMessage));
+}
 
 var lastName = document.getElementById("last-name");
 var birthDate = document.getElementById("birth-date");
 var birthPlace = document.getElementById("birth-place");
 var male = document.getElementById("male");
 var female = document.getElementById("female");
-var name = document.getElementById("name");
+var nam = document.getElementById("name");
 var yearOld = document.getElementById("year-old");
 var region = document.getElementById("region");
 var job = document.getElementById("job");
 var timeInMonths = document.getElementById("time-in-months");
 var cargo = document.getElementById("cargo");
 var timeInOrg = document.getElementById("time-in-org");
+
+window.addEventListener("load", function(){
+    createInputCheck(checkRegiones, "div-check-input", divChkRegiones);
+    createInputCheck(checkSocialSector, "div-check-input", divSocialSector);
+
+    var allInput = document.querySelectorAll("input[type=text]");
+    for (var i = 0; i < allInput.length; i++) {
+      allInput[i].addEventListener("blur", capitalLetter);
+    }
+
+    var letterType = document.querySelectorAll(".letter");
+    console.log(letterType);
+    for (var i = 0; i < letterType.length; i++) {
+      letterType[i]. addEventListener("keydown", function(){
+        onlyLetter("Solo ingresa letras.")
+      })
+    }
+});
